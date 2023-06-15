64c33c5e66883762d582726adc58e4dbab1cfc80
むずでょ <muzudho1@gmail.com>
2023-06-16 02:01:34 +0900

    Add comments

--- html/search/properties_b.js
+++ html/search/properties_b.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['keepwaiting_0',['keepWaiting',['../d4/d2d/class_r_p_g_maker_1_1_codebase_1_1_runtime_1_1_title_1_1_wait_for_animation.html#acd4d533351feb3fbd4d27d12ef7fc8cf',1,'RPGMaker::Codebase::Runtime::Title::WaitForAnimation']]],
   ['key_1',['Key',['../dd/db6/class_r_p_g_maker_1_1_codebase_1_1_editor_1_1_common_1_1_addon_u_i_util_1_1_key_visual_element.html#a5f0edbb2184c9960a922f21de5952f29',1,'RPGMaker::Codebase::Editor::Common::AddonUIUtil::KeyVisualElement']]],
-  ['keyitem_2',['keyItem',['../d7/da4/class_r_p_g_maker_1_1_codebase_1_1_runtime_1_1_common_1_1_text_manager.html#ae908f516c9949e1dca8069a9e9b75807',1,'RPGMaker::Codebase::Runtime::Common::TextManager']]],
-  ['keyitem_3',['KeyItem',['../d9/d2c/class_r_p_g_maker_1_1_codebase_1_1_core_system_1_1_knowledge_1_1_data_model_1_1_word_definition_38d0d7732161d2bbedb9edf0cada8aab.html#a1faa809a428f88894a5b572e819f5b8c',1,'RPGMaker::Codebase::CoreSystem::Knowledge::DataModel::WordDefinition::WordDefinitionDataModel::Commands']]]
+  ['keyitem_2',['KeyItem',['../d9/d2c/class_r_p_g_maker_1_1_codebase_1_1_core_system_1_1_knowledge_1_1_data_model_1_1_word_definition_38d0d7732161d2bbedb9edf0cada8aab.html#a1faa809a428f88894a5b572e819f5b8c',1,'RPGMaker::Codebase::CoreSystem::Knowledge::DataModel::WordDefinition::WordDefinitionDataModel::Commands']]],
+  ['keyitem_3',['keyItem',['../d7/da4/class_r_p_g_maker_1_1_codebase_1_1_runtime_1_1_common_1_1_text_manager.html#ae908f516c9949e1dca8069a9e9b75807',1,'RPGMaker::Codebase::Runtime::Common::TextManager']]]
 ];
